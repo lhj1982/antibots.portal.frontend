b7600b84e1ca5ecab7e7775e4cb35be32cf71063
Bradley Shao <Bradley.shao@nike.com>
2024-03-13 16:33:58 +0800

    optimise code

--- lib/getWebbRuleList.tsx
+++ lib/getWebbRuleList.tsx
@@ -1,4 +1,5 @@
 import { BACKEND_HOST, WEBBRULE_GETLIST_PATH } from "@/utils/constants";
+import { useUserStore } from "@/zustand/userStore";
 import axios from "axios";
 
 export default async function getWebbRuleList() {
